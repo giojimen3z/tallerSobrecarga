f432760e35139952a76250f60a3fd0bc9d1d0132
dazarate1 <62047011+dazarate1@users.noreply.github.com>
2020-09-23 18:15:05 -0500

    sobrecarga y actualizacion de cumpleaños

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -5,8 +7,19 @@ public class Main {
 
         Boss gio = new Boss();
         gio.setSalary((double) 500000);
-        System.out.println("salario = " + gio.getSalary());
+        System.out.println("salario gio = " + gio.getSalary());
 
+        Employee carol= new Employee();
+        carol.setSalary((double) 500000);
+        System.out.println("salario carol = " + carol.getSalary());
 
+        Scholar david= new Scholar();
+        david.setSalary((double) 500000);
+        System.out.println("salario david = " + david.getSalary());
+
+        david.setAge(26);
+        david.BirthDay(0);
+
+        System.out.println("edad de cumpleaños = " + david.getAge());
     }
 }
